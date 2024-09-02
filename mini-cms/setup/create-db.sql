e556728765b0dc7d608a8b9f94ccf919f082721d
tomoaoihana <tamu.kusanishi@gmail.com>
2024-09-02 09:40:46 +0900

    レコードの挿入

--- mini-cms/setup/create-db.sql
+++ mini-cms/setup/create-db.sql
@@ -18,6 +18,44 @@ CREATE TABLE posts(
 
 CREATE TABLE categories(
   id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY ,
- category_name VARCHAR(60) NOT NULL,
+ category_name VARCHAR(60) NOT NULL
  
 ) engine=InnoDB default charset=utf8;
+
+-- サンプルレコードの挿入
+
+INSERT INTO posts(
+  title,
+  content,
+  category_id,
+  created
+)values(
+  'Webサイトを公開しました。',
+  '本日、Webサイトを公開しました。お楽しみ下さい。',
+  1,
+  '2018-04-01 10:00:00'
+),
+(
+  '春の握手会について',
+  '	4月21日（土）、 22日（日）の2日間、握手会を開催致します。',
+  2,
+  '2018-04-16 10:00:00'
+),
+(
+  'ゴールデンウィーク休暇について',
+  '4月28日（土） 〜 5月6日（日）までの9日間は、休みを頂戴いたします。',
+  1,
+  '2018-04-20 10:00:00'
+);
+
+INSERT INTO categories(
+  category_name
+)values(
+  'お知らせ'
+),
+(
+  'イベント情報'
+),
+(
+  '活動報告'
+);
