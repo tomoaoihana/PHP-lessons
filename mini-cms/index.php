fcf62c69ec6d19f1d77f6a2fda5d4588242c1631
tomoaoihana <tamu.kusanishi@gmail.com>
2024-09-03 09:22:26 +0900

    DBに接続

---
+++ mini-cms/index.php
@@ -0,0 +1,20 @@
+<?php
+// データベースに接続
+$dbh = new PDO('mysql:host=localhost;dbname=mini_cms_app;charset=utf8', 'root', 'root');
+echo '接続成功！';
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>新着記事一覧</title>
+</head>
+
+<body>
+  <h1>新着情報</h1>
+</body>
+
+</html>
