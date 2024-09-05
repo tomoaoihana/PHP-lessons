8b7f97121055dbcb2dfce40dbb1f6ff836ff44d1
tomoaoihana <tamu.kusanishi@gmail.com>
2024-09-05 09:02:52 +0900

    DB情報は別ファイルにしておく

---
+++ mini-cms/inc/config.php
@@ -0,0 +1,8 @@
+<?php
+
+//DBの定数を定義していく
+
+define('DB_NAME','mini_cms_app');//DB名
+define('DB_USER','root');//DBユーザー名
+define('DB_PASSWORD','root');//DBパスワード
+define('DSN','mysql:host=localhost;dbname=' . DB_NAME . ';charset=utf8');//DSN
