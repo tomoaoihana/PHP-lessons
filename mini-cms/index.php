484b0fab53074a5bc1c421f26f9de8fe684decb6
tomoaoihana <tamu.kusanishi@gmail.com>
2024-09-17 06:02:22 +0900

    例外処理の記述

--- mini-cms/index.php
+++ mini-cms/index.php
@@ -2,9 +2,23 @@
 //ファイルの読み込み
 require_once('inc/config.php');
 
-// データベースに接続
-$dbh = new PDO(DSN, DB_USER, DB_PASSWORD);
-echo '接続成功！';
+//例外処理（try-catch文）
+try{
+  // データベースに接続
+  $dbh = new PDO(DSN, DB_USER, DB_PASSWORD);
+
+  //エラー発生時に『PDOException』という例外クラスのオブジェクトがスロー（投げる）されるように設定
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo '接続成功！';
+  
+}catch(PDOException $e){
+  //例外発生時の処理
+ echo 'エラー！' . $e->getMessage(); //getMessage()メソッドでエラーメッセージを取得 無害かした方がいいい
+  exit();
+
+}
+
+
 ?>
 
 <!DOCTYPE html>
